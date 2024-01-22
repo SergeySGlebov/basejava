566d53e8f07e4f2f6c468ff55b545f01b4097ec5
CJloHuK <askabelin@gmail.com>
2024-01-22 15:44:32 +0300

    HW03 add SortedArrayStorage, modify AbstractArrayStorage with template method

--- src/com/urise/webapp/storage/ArrayStorage.java
+++ src/com/urise/webapp/storage/ArrayStorage.java
@@ -7,87 +7,25 @@ import java.util.Arrays;
 /**
  * Array based storage for Resumes
  */
-public class ArrayStorage implements Storage {
+public class ArrayStorage extends AbstractArrayStorage {
 
-    private static final int STORAGE_LIMIT = 10000;
-
-    private Resume[] storage = new Resume[STORAGE_LIMIT];
-    private int size = 0;
-
-    public void clear() {
-        Arrays.fill(storage, 0, size, null);
-        size = 0;
-    }
-
-    public void update(Resume r) {
-        int id = getIdOfElement(r.getUuid());
-        if (needToExist(id, r.getUuid())) {
-            storage[id] = r;
-        }
-    }
-
-    public void save(Resume r) {
-        int id = getIdOfElement(r.getUuid());
-        if (doNotNeedToExist(id, r.getUuid())) {
-            if (size < storage.length) {
-                storage[size++] = r;
-            }
-        }
-    }
-
-    public Resume get(String uuid) {
-        int id = getIdOfElement(uuid);
-        if (needToExist(id, uuid)) {
-            return storage[id];
-        }
-        return null;
-    }
-
-    public void delete(String uuid) {
-        int id = getIdOfElement(uuid);
-        if (needToExist(id, uuid)) {
-            size--;
-            for (int i = id; i < size; i++) {
-                storage[i] = storage[id + 1];
-            }
-        }
-    }
-
-    /**
-     * @return array, contains only Resumes in storage (without null)
-     */
-    public Resume[] getAll() {
-        return Arrays.copyOf(storage, size);
-    }
-
-    public int size() {
-        return size;
-    }
-
-    private int getIdOfElement(String uuid) {
+    protected int getIndex(String uuid) {
         for (int i = 0; i < size; i++) {
-            if (storage[i].getUuid().equals(uuid)) {
+            if (uuid.equals(storage[i].getUuid())) {
                 return i;
             }
         }
         return -1;
     }
 
-    private boolean needToExist(int id, String uuid) {
-        if (id >= 0) {
-            return true;
-        } else {
-            System.out.println("ERROR: resume " + uuid + " does not exist in storage");
-        }
-        return false;
+    @Override
+    protected void deleteFromStorage(int index) {
+        storage[index] = storage[size - 1];
+        storage[size - 1] = null;
     }
 
-    private boolean doNotNeedToExist(int id, String uuid) {
-        if (id < 0) {
-            return true;
-        } else {
-            System.out.println("ERROR: resume " + uuid + " already exist in storage");
-        }
-        return false;
+    @Override
+    protected void saveToStorage(Resume r, int index) {
+        storage[size] = r;
     }
 }
---
+++ src/com/urise/webapp/storage/SortedArrayStorage.java
@@ -0,0 +1,30 @@
+package com.urise.webapp.storage;
+
+import com.urise.webapp.model.Resume;
+
+import java.util.Arrays;
+
+public class SortedArrayStorage extends AbstractArrayStorage {
+
+    @Override
+    protected int getIndex(String uuid) {
+        Resume searchKey = new Resume();
+        searchKey.setUuid(uuid);
+        return Arrays.binarySearch(storage, 0, size, searchKey);
+    }
+
+    @Override
+    protected void deleteFromStorage(int index) {
+        for (int i = index; i < size - 1; i++) {
+            storage[i] = storage[i + 1];
+        }
+    }
+
+    @Override
+    protected void saveToStorage(Resume r, int index) {
+        for (int i = size; i >= index; i--) {
+            storage[i] = storage[i - 1];
+        }
+        storage[index - 1] = r;
+    }
+}
