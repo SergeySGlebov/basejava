d38fafb038b12c8adc438ec7a37ca1c397d21507
CJloHuK <askabelin@gmail.com>
2024-02-05 18:23:55 +0300

    Lesson07 logging

--- src/com/urise/webapp/storage/AbstractStorage.java
+++ src/com/urise/webapp/storage/AbstractStorage.java
@@ -6,25 +6,33 @@ import com.urise.webapp.model.Resume;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public abstract class AbstractStorage<SK> implements Storage {
 
+    private static final Logger LOG = Logger.getLogger(AbstractStorage.class.getName());
+
     public void update(Resume r) {
+        LOG.info("Update " + r);
         SK searchKey = getExistedKey(r.getUuid());
         doUpdate(r, searchKey);
     }
 
     public void save(Resume r) {
+        LOG.info("Save " + r);
         SK searchKey = getNotExistedKey(r.getUuid());
         doSave(r, searchKey);
     }
 
     public Resume get(String uuid) {
+        LOG.info("Get " + uuid);
         SK searchKey = getExistedKey(uuid);
         return doGet(searchKey);
     }
 
     public void delete(String uuid) {
+        LOG.info("Delete " + uuid);
         SK searchKey = getExistedKey(uuid);
         doDelete(searchKey);
     }
@@ -32,6 +40,7 @@ public abstract class AbstractStorage<SK> implements Storage {
     private SK getExistedKey(String uuid) {
         SK searchKey = getSearchKey(uuid);
         if (!isExist(searchKey)) {
+            LOG.warning("Resume " + uuid + " not exist");
             throw new NotExistStorageException(uuid);
         }
         return searchKey;
@@ -40,6 +49,7 @@ public abstract class AbstractStorage<SK> implements Storage {
     private SK getNotExistedKey(String uuid) {
         SK searchKey = getSearchKey(uuid);
         if (isExist(searchKey)) {
+            LOG.warning("Resume " + uuid + " already exist");
             throw new ExistStorageException(uuid);
         }
         return searchKey;
@@ -47,6 +57,7 @@ public abstract class AbstractStorage<SK> implements Storage {
 
     @Override
     public List<Resume> getAllSorted() {
+        LOG.info("GetAllSorted");
         List<Resume> list = doCopyAll();
         Collections.sort(list);
         return list;
